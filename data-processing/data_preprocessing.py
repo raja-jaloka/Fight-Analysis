a407a8c5b42c88766bcced471fb5b820fdad3f00
Ludwig_Bonaparte <yuvraj100706@gmail.com>
2026-08-09 22:48:45 +0530

    Performed metric checks for LogisticRegresion, Decision Trees and Random Forest Classifier

--- data-processing/data_preprocessing.py
+++ data-processing/data_preprocessing.py
@@ -1,12 +1,12 @@
 import pandas as pd 
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import quantile_transform
+from sklearn.preprocessing import QuantileTransformer
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import precision_score, accuracy_score
+from sklearn.metrics import precision_score, accuracy_score, confusion_matrix,recall_score
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 df=pd.read_csv("processed_data.csv")
 
@@ -19,28 +19,63 @@ X=df.drop(columns=["outcome","sr_no"])
 #print(dfna[dfna>0])
 
 
-pipe=Pipeline(steps=[('scaler',StandardScaler()),('model',LogisticRegression())])
-#pipe_=Pipeline(steps=[('scaler',quantile_transform()),('model',LogisticRegression())])
+pipe=Pipeline(steps=[('scaler',StandardScaler()),('model',LogisticRegression(max_iter=1000))])
 pipe1=Pipeline(steps=[("scaler",StandardScaler()),("model",DecisionTreeClassifier())])
-#pipe_1=Pipeline(steps=[("scaler",quantile_transform()),("model",DecisionTreeClassifier())])
-pipe3=Pipeline(steps=[("scaler",StandardScaler()),("model",MLPClassifier())])
-#pipe_3=Pipeline(steps=[("scaler",quantile_transform()),("model",MLPClassifier())])
-
+#pipe3=Pipeline(steps=[("scaler",StandardScaler()),("model",MLPClassifier())])
+pipe_=Pipeline(steps=[("transformer",QuantileTransformer(output_distribution="normal")),("model",LogisticRegression())])
+pipe3=Pipeline(steps=[("scaler",QuantileTransformer(output_distribution="normal")),("clf",RandomForestClassifier(n_estimators=500,max_depth=None,random_state=42))])
 X_train,X_test,Y_train,Y_test=train_test_split(X,Y,random_state=42,test_size=0.2)
 pipe.fit(X_train,Y_train)
 pipe1.fit(X_train,Y_train)
+pipe_.fit(X_train,Y_train)
 pipe3.fit(X_train,Y_train)
 #print(pipe.predict(X_test))
+print("X:", X.shape)
+print("Y:", Y.shape)
+
+print("X_train:", X_train.shape)
+print("X_test:", X_test.shape)
+
+print("Y_train:", Y_train.shape)
+print("Y_test:", Y_test.shape)
 
 print("Logistic Regression metrics")
-print(accuracy_score(Y_test,pipe.predict(X_test)))
-print(precision_score(Y_test,pipe.predict(X_test)))
+print("Accuracy Score:-",end="---")
+print(accuracy_score(Y_test,pipe.predict(X_test))*100,end="%\n")
+print("Precision Score:-",end="---")
+print(precision_score(Y_test,pipe.predict(X_test))*100,end="%\n")
+print(confusion_matrix(Y_test,pipe.predict(X_test)))
+print("Recall Score:-",end="---")
+print(recall_score(Y_test,pipe.predict(X_test))*100)
+print("----quantileTransformer----")
+print("Accuracy Score:-",end="---")
+print(accuracy_score(Y_test,pipe_.predict(X_test))*100,end="%\n")
+print("Precision Score:-",end="---")
+print(precision_score(Y_test,pipe_.predict(X_test))*100,end="%\n")
+print(confusion_matrix(Y_test,pipe_.predict(X_test)))
+print("Recall Score:-",end="---")
+print(recall_score(Y_test,pipe_.predict(X_test))*100)
 print("DecisionTreeClassifier metrics")
-print(accuracy_score(Y_test,pipe1.predict(X_test)))
-print(precision_score(Y_test,pipe1.predict(X_test)))
-print("MLP metrics")
-print(accuracy_score(Y_test,pipe3.predict(X_test)))
-print(precision_score(Y_test,pipe3.predict(X_test)))
+print("Accuracy Score:-",end="---")
+print(accuracy_score(Y_test,pipe1.predict(X_test))*100,end="%\n")
+print("Precision Score:-",end="---")
+print(precision_score(Y_test,pipe1.predict(X_test))*100,end="%\n")
+print(confusion_matrix(Y_test,pipe1.predict(X_test)))
+print("Recall Score:-",end="---")
+print(recall_score(Y_test,pipe1.predict(X_test))*100)
 
+print("RandomForestClassifier metrics")
+print("Accuracy Score:-",end="---")
+print(accuracy_score(Y_test,pipe3.predict(X_test))*100,end="%\n")
+print("Precision Score:-",end="---")
+print(precision_score(Y_test,pipe3.predict(X_test))*100,end="%\n")
+print(confusion_matrix(Y_test,pipe3.predict(X_test)))
+print("Recall Score:-",end="---")
+print(recall_score(Y_test,pipe3.predict(X_test))*100)
+
+#print("MLP metrics")
+#print(accuracy_score(Y_test,pipe3.predict(X_test)))
+#print(precision_score(Y_test,pipe3.predict(X_test)))
 #Conclusion1: round-wise stats improve accuracy by only a minute percentage with the heavy reliance on overall stats.
 #Conclusion2: LogisticRegression>DecisionTreeClassifier,MLP
+#Conclusion 3 : better results with Logistics regression + quantile Transformer

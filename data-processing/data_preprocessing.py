64accf6c264e64f3f246d82ad67679e7d6552d44
Ludwig_Bonaparte <yuvraj100706@gmail.com>
2026-08-11 17:01:28 +0530

    Computed logloss and plotted Calibration Curves for model selection

--- data-processing/data_preprocessing.py
+++ data-processing/data_preprocessing.py
@@ -12,6 +12,8 @@ from sklearn.metrics import roc_curve #type: ignore
 from sklearn.metrics import RocCurveDisplay #type: ignore
 from xgboost import XGBClassifier 
 import time 
+from sklearn.calibration import CalibrationDisplay
+from sklearn.metrics import log_loss
 
 #Input CSV File
 df=pd.read_csv("processed_data.csv")
@@ -41,6 +43,8 @@ print(confusion_matrix(Y_test,pipe.predict(X_test)))
 print("Recall Score:-",end="---")
 print(recall_score(Y_test,pipe.predict(X_test))*100)
 RocCurveDisplay.from_estimator(pipe,X_test,Y_test)
+CalibrationDisplay.from_estimator(pipe,X_test,Y_test)
+print(f'logloss: {log_loss(y_true=Y_test,y_pred=pipe.predict(X_test))}')
 print(f'Model 1 exec time: {time.time()-a}')
 
 a=time.time()
@@ -56,10 +60,12 @@ print(confusion_matrix(Y_test,pipe_.predict(X_test)))
 print("Recall Score:-",end="---")
 print(recall_score(Y_test,pipe_.predict(X_test))*100)
 RocCurveDisplay.from_estimator(pipe_,X_test,Y_test)
+CalibrationDisplay.from_estimator(pipe_,X_test,Y_test)
+print(f'logloss: {log_loss(y_true=Y_test,y_pred=pipe_.predict(X_test))}')
 print(f'Model 2 exec time: {time.time()-a}')
 
 a=time.time()
-#Model 3: Decision Tree Classifier + Quantile Transformer
+#Model 3: Decision Tree Classifier + Standard Scaler
 pipe1=Pipeline(steps=[("scaler",StandardScaler()),("model",DecisionTreeClassifier())])
 pipe1.fit(X_train,Y_train)
 print("DecisionTreeClassifier metrics")
@@ -71,10 +77,12 @@ print(confusion_matrix(Y_test,pipe1.predict(X_test)))
 print("Recall Score:-",end="---")
 print(recall_score(Y_test,pipe1.predict(X_test))*100)
 RocCurveDisplay.from_estimator(pipe1,X_test,Y_test)
+CalibrationDisplay.from_estimator(pipe1,X_test,Y_test)
+print(f'logloss: {log_loss(y_true=Y_test,y_pred=pipe1.predict(X_test))}')
 print(f'Model 3 exec time: {time.time()-a}')
 
 a=time.time()
-#Model 4: RandomForestClassifier + Quantile Transformer
+#Model 4: RandomForestClassifier + Standard Scaler
 #pipe3=Pipeline(steps=[("scaler",QuantileTransformer(output_distribution="normal")),("clf",RandomForestClassifier(n_estimators=500,max_depth=None,random_state=42))])
 pipe3=Pipeline(steps=[("scaler",StandardScaler()),("clf",RandomForestClassifier(n_estimators=500,max_depth=None,random_state=42))])
 pipe3.fit(X_train,Y_train)
@@ -87,10 +95,12 @@ print(confusion_matrix(Y_test,pipe3.predict(X_test)))
 print("Recall Score:-",end="---")
 print(recall_score(Y_test,pipe3.predict(X_test))*100)
 RocCurveDisplay.from_estimator(pipe3,X_test,Y_test)
+CalibrationDisplay.from_estimator(pipe3,X_test,Y_test)
+print(f'logloss: {log_loss(y_true=Y_test,y_pred=pipe3.predict(X_test))}')
 print(f'Model 4 exec time: {time.time()-a}')
 
 a=time.time()
-#Model 5: XGBClassifier + Quantile Transformer
+#Model 5: XGBClassifier + Standard Scaler
 #pipe4=Pipeline(steps=[("transformer",QuantileTransformer(output_distribution="normal")),("model",XGBClassifier(n_estimators=500,max_depth=None,learning_rate=1,objective="binary:logistic"))])
 pipe4=Pipeline(steps=[("scaler",StandardScaler()),("model",XGBClassifier(n_estimators=500,max_depth=None,learning_rate=1,objective="binary:logistic"))])
 pipe4.fit(X_train,Y_train)
@@ -103,6 +113,8 @@ print(confusion_matrix(Y_test,pipe4.predict(X_test)))
 print("Recall Score:-",end="---")
 print(recall_score(Y_test,pipe4.predict(X_test))*100)
 RocCurveDisplay.from_estimator(pipe4,X_test,Y_test)
+CalibrationDisplay.from_estimator(pipe4,X_test,Y_test)
+print(f'logloss: {log_loss(y_true=Y_test,y_pred=pipe4.predict(X_test))}')
 print(f'Model 5 exec time: {time.time()-a}')
 
 plt.show()
@@ -116,3 +128,4 @@ plt.show()
 #Conclusion 4: better results from RandomForestClasssifier based on ROC-AUC + quantile transformer 
 #Conclusion 5: XGB's AUC ~= LR+QT's 
 #Conclusion 6: LR+QT's execution time < RFC's exec time with <1% accuracy change so we choose model 2
+#Conclusion 7 : Model 2 > Model 4 in terms of Calibration and log loss
